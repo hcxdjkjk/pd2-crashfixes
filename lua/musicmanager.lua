f781e41d702bb5ce4b894d3b624664339243158e
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-08-21 19:58:20 +0700

    Create musicmanager.lua

---
+++ lua/musicmanager.lua
@@ -0,0 +1,20 @@
+local _track_listen_start = MusicManager.track_listen_start
+
+function MusicManager:track_listen_start(event, track)
+	_track_listen_start(self, event, tostring(track))
+end
+
+function MusicManager:track_listen_stop()
+	Global.music_manager.source:post_event("stop_all_music")
+	self._current_event = nil
+	self._current_track = nil
+	self._skip_play = nil
+end
+
+function MusicManager:stop_listen_all()
+	Global.music_manager.source:post_event("stop_all_music")
+	self._current_music_ext = nil
+	self._current_event = nil
+	self._current_track = nil
+	self._skip_play = nil
+end

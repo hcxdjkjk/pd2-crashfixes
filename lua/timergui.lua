3a1ad8729808394ff7b7c40daecc6d0d27c42534
hcxdjkjk <d99258791@gmail.com>
2025-08-21 17:49:13 +0500

    Update timergui.lua

--- lua/timergui.lua
+++ lua/timergui.lua
@@ -125,9 +125,9 @@ function TimerGui:update(unit, t, dt)
 	self._current_timer = (self._current_timer or 5) - dt / dt_mod
 	self._time_left = (self._current_timer or 5) * dt_mod
 
-	local value = math.floor(self._time_left)
-	if value ~= self.fs_current_time then
-		self.fs_current_time = value
+	local value = math.floor(self._time_left or 5)
+	if value ~= self._current_timer then
+		self._current_timer = value
 		if self._show_seconds then
 			self._gui_script.time_text:set_text(math.floor(self._time_left or self._current_timer) .. " " .. managers.localization:text("prop_timer_gui_seconds"))
 		else

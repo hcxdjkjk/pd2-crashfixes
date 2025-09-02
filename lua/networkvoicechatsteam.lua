5a3285d407bb7b19538f6795ee17b4b9311bc49c
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-09-02 19:21:17 +0700

    Create networkvoicechatsteam.lua

---
+++ lua/networkvoicechatsteam.lua
@@ -0,0 +1,9 @@
+local orig_func_NetworkVoiceChatSTEAM_update = NetworkVoiceChatSTEAM.update
+function NetworkVoiceChatSTEAM:update()
+    local playing = self.handler:get_voice_receivers_playing()
+		for id, pl in pairs(playing) do
+		    if pl and self._users_talking[id] and self._users_talking[id].time then\
+          orig_func_NetworkVoiceChatSTEAM_update(self)
+        end
+    end
+end

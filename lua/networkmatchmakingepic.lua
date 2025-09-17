536adf944b7bc12e5c377f8d53af44b8042e8d74
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-09-17 21:02:14 +0700

    Update networkmatchmakingepic.lua

--- lua/networkmatchmakingepic.lua
+++ lua/networkmatchmakingepic.lua
@@ -1,5 +1,6 @@
 local _is_server_ok = NetworkMatchMakingEPIC.is_server_ok
 function NetworkMatchMakingEPIC:is_server_ok(friends_only, room, attributes_list, is_invite)
+	local lobby = EpicMM:lobby(room.room_id)
 	for i = 1, 8 do
 		if type(attributes_list.numbers[i]) ~= "number" then return false end
 	    if attributes_list.numbers[i] == nil then
@@ -8,6 +9,9 @@ function NetworkMatchMakingEPIC:is_server_ok(friends_only, room, attributes_list
 	end
 	if attributes_list.numbers[5] >= 4 then
 	   	return false
+	end
+	if not tonumber(lobby:key_value("difficulty)) then
+		lobby:key_value("difficulty) = -1
 	end
 	return _is_server_ok(self, friends_only, room, attributes_list, is_invite)
 end

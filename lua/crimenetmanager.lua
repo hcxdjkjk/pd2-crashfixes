bc7611f05c5fee337fd1f33dfc414339421e6188
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-09-18 00:13:27 +0700

    Update crimenetmanager.lua

--- lua/crimenetmanager.lua
+++ lua/crimenetmanager.lua
@@ -3,5 +3,8 @@ function CrimeNetGui:add_server_job(data)
 	if not (tonumber(data.difficulty_id)) then
 		data.difficulty_id = -1
 	end
+	if data.mutators and type(data.mutators) == "table" and table.empty(data.mutators) then --stealed but idc
+    	data.mutators = false
+  	end
 	_add_server_job(self, data)
 end

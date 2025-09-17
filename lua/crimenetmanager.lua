3b43445022d3eb1f8cc0632b3161d95d6467229f
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-09-17 20:57:24 +0700

    Update crimenetmanager.lua

--- lua/crimenetmanager.lua
+++ lua/crimenetmanager.lua
@@ -1,7 +1,7 @@
 local _add_server_job = CrimeNetGui.add_server_job
 function CrimeNetGui:add_server_job(data)
 	if not (tonumber(data.difficulty_id)) then
-		data.difficulty_id = 0
+		data.difficulty_id = -1
 	end
 	_add_server_job(self, data)
 end

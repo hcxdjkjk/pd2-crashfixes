35e5a41b20f59177316fe8a2d080f570d6130806
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-09-17 17:40:54 +0700

    Create networkmatchmakingepic.lua

---
+++ lua/networkmatchmakingepic.lua
@@ -0,0 +1,10 @@
+local _is_server_ok = NetworkMatchMakingEPIC.is_server_ok
+function NetworkMatchMakingEPIC:is_server_ok(friends_only, room, attributes_list, is_invite)
+	for i = 1, 8 do
+		if type(attributes_list.numbers[i]) ~= "number" then return false end
+	    if attributes_list.numbers[i] == nil then
+	    	attributes_list.numbers[i] = 0
+	    end
+	end
+	return _is_server_ok(self, friends_only, room, attributes_list, is_invite)
+end

7d01adf2d21b67f55a1a95a3c2ed01ea63e46dd2
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-09-17 21:04:46 +0700

    Update networkmatchmakingepic.lua

--- lua/networkmatchmakingepic.lua
+++ lua/networkmatchmakingepic.lua
@@ -10,8 +10,8 @@ function NetworkMatchMakingEPIC:is_server_ok(friends_only, room, attributes_list
 	if attributes_list.numbers[5] >= 4 then
 	   	return false
 	end
-	if not tonumber(lobby:key_value("difficulty")) then
-		lobby:key_value("difficulty) = -1
+	if not tonumber(attributes_list.numbers[2]) then
+	   	attributes_list.numbers[2] = -1
 	end
 	return _is_server_ok(self, friends_only, room, attributes_list, is_invite)
 end

79a996859ad9bcc036c581dfec6c920913b932c8
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-09-26 16:23:55 +0700

    Update unitnetworkhandler.lua

--- lua/unitnetworkhandler.lua
+++ lua/unitnetworkhandler.lua
@@ -12,12 +12,3 @@ function UnitNetworkHandler:sync_melee_start(unit, hand, sender)
 		unit:movement():sync_melee_start(hand)
 	end
 end
-
---[string "lib/network/handlers/unitnetworkhandler.lua"]:3503: attempt to index local 'unit' (a nil value)
-function UnitNetworkHandler:sync_drill_upgrades(unit, autorepair_level_1, autorepair_level_2, drill_speed_level, silent, reduced_alert, sender_rpc)
-	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character_and_sender(unit, sender_rpc) then return end
-	local base_ext = unit and alive(unit) and unit:base()
-	if base_ext and base_ext.set_skill_upgrades then
-		base_ext:set_skill_upgrades(Drill.create_upgrades(autorepair_level_1, autorepair_level_2, drill_speed_level, silent, reduced_alert))
-	end
-end

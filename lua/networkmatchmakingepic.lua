34a367f732b4eb64ae255636b5ba8c9f4bb923fc
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-11-13 18:31:10 +0700

    Update networkmatchmakingepic.lua

--- lua/networkmatchmakingepic.lua
+++ lua/networkmatchmakingepic.lua
@@ -2,15 +2,8 @@ local _is_server_ok = NetworkMatchMakingEPIC.is_server_ok
 function NetworkMatchMakingEPIC:is_server_ok(friends_only, room, attributes_list, is_invite)
 	for z, v in pairs(attributes_list.numbers) do
 		if type(v) ~= "number" then
-			return false
-		end
-		if v == nil then
 			attributes_list.numbers[z] = 0
 		end
-	end
-	
-	if attributes_list.numbers[5] >= 4 then
-	   	return false
 	end
 	
 	return _is_server_ok(self, friends_only, room, attributes_list, is_invite)

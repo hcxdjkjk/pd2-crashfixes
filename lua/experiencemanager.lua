1572dd2f8791602202a64db22b8b3b2a9c434f68
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-12-28 14:53:31 +0700

    Create experiencemanager.lua

---
+++ lua/experiencemanager.lua
@@ -0,0 +1,15 @@
+local _get_xp_by_params = ExperienceManager.get_xp_by_params
+function ExperienceManager:get_xp_by_params(params)
+	if type(params.mission_xp) ~= "number" then
+		params.mission_xp = 0
+	end
+	return _get_xp_by_params(self, params)
+end
+
+local _get_contract_xp_by_stars = ExperienceManager.get_contract_xp_by_stars
+function ExperienceManager:get_contract_xp_by_stars(job_id, job_stars, risk_stars, professional, job_days, extra_params)
+	if type(extra_params.mission_xp) ~= "number" then
+		extra_params.mission_xp = 0
+	end
+	return _get_contract_xp_by_stars(self, job_id, job_stars, risk_stars, professional, job_days, extra_params)
+end

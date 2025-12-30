a241bda9768980c5d176e1bc889f4c6b442b3d13
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2025-12-30 21:06:28 +0700

    Update networkmatchmakingepic.lua

--- lua/networkmatchmakingepic.lua
+++ lua/networkmatchmakingepic.lua
@@ -5,6 +5,10 @@ function NetworkMatchMakingEPIC:is_server_ok(friends_only, room, attributes_list
 			attributes_list.numbers[z] = 0
 		end
 	end
+
+	if type(attributes_list.mods) ~= "string" or attributes_list.mods == "empty" then
+		attributes_list.mods = "7d66a433be3a1fe2"
+	end
 	
 	return _is_server_ok(self, friends_only, room, attributes_list, is_invite)
 end

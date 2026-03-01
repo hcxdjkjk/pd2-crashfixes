1f0a4e4ffb99d9bc102d476f35d545f34b868ced
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2026-03-01 21:31:23 +0700

    Update copactionshoot.lua

--- lua/copactionshoot.lua
+++ lua/copactionshoot.lua
@@ -1,16 +1,21 @@
--- [string "lib/units/enemies/cop/actions/upper_body/copa..."]:402: attempt to index field '_weapon_base' (a nil value)
 local __update = CopActionShoot.update
+local __get_unit_shoot_pos = CopActionShoot._get_unit_shoot_pos
+
+--[string "lib/units/enemies/cop/actions/upper_body/copa..."]:402: attempt to index field '_weapon_base' (a nil value)
 function CopActionShoot:update(...)
     if self._weapon_base then __update(self, ...) end
 end
--- [string "lib/units/enemies/cop/actions/upper_body/copa..."]:658: attempt to perform arithmetic on local 't' (a userdata value)
-local __get_unit_shoot_pos = CopActionShoot._get_unit_shoot_pos
+
 function CopActionShoot:_get_unit_shoot_pos(t, pos, ...)
+    --[string "lib/units/enemies/cop/actions/upper_body/copa..."]:658: attempt to perform arithmetic on local 't' (a userdata value)
     if type(t) == "userdata" and type(pos) == "number" then
         local save_t = t
         local save_pos = pos
         t = save_pos
         pos = save_t
     end
-    return __get_unit_shoot_pos(self, t, pos, ...)
+    --[string "lib/units/enemies/cop/actions/upper_body/copa..."]:674: attempt to index local 'w_tweak' (a number value)
+    if type(w_tweak) == "table" then
+        return __get_unit_shoot_pos(self, t, pos, ...)
+    end
 end

ddb52e98d6f86dff7737ea104ad7452cbec2ea23
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2026-03-28 00:27:42 +0700

    Update networkmatchmakingepic.lua

--- lua/networkmatchmakingepic.lua
+++ lua/networkmatchmakingepic.lua
@@ -1,5 +1,17 @@
 local _is_server_ok = NetworkMatchMakingEPIC.is_server_ok
 function NetworkMatchMakingEPIC:is_server_ok(friends_only, room, attributes_list, is_invite)
+	if type(room) ~= "table" then 
+		return false 
+	end
+
+	if type(attributes_list) ~= "table" then
+		return false
+	end
+
+	if type(friends_only) ~= "boolean" then
+		friends_only = false
+	end
+	
 	for z, v in pairs(attributes_list.numbers) do
 		if type(v) ~= "number" then
 			attributes_list.numbers[z] = 0

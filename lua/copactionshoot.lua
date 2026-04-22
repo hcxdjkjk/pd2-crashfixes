c8e9d2caed3e2b1a00c5cae196d75deb93676a96
s1rdenis <88074497+s1rdenis@users.noreply.github.com>
2026-04-22 23:59:50 +0700

    Update copactionshoot.lua

--- lua/copactionshoot.lua
+++ lua/copactionshoot.lua
@@ -3,13 +3,14 @@ local __get_unit_shoot_pos = CopActionShoot._get_unit_shoot_pos
 
 --[string "lib/units/enemies/cop/actions/upper_body/copa..."]:402: attempt to index field '_weapon_base' (a nil value)
 function CopActionShoot:update(...)
-    if self._weapon_base then __update(self, ...) end
+    if self._weapon_base then 
+        __update(self, ...) 
+    end
 end
 
+--[string "lib/units/enemies/cop/actions/upper_body/copa..."]:674: attempt to index local 'w_tweak' (a number value)
 function CopActionShoot:_get_unit_shoot_pos(t, pos, dis, w_tweak, ...)
-    --[string "lib/units/enemies/cop/actions/upper_body/copa..."]:674: attempt to index local 'w_tweak' (a number value)
     if type(w_tweak) == "table" then
         return __get_unit_shoot_pos(self, t, pos, dis, w_tweak, ...)
     end
 end
-
